5ca3dcef55ad66c0349d538d955712ee6620a1f3
Seung-gi Min <110602191+seunGit@users.noreply.github.com>
2022-12-01 09:50:28 +0900

    Delete basic_crud_10 directory

--- basic_crud_10/src/main/java/com/example/basic_crud_10/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.basic_crud_10.repository;
-
-import com.example.basic_crud_10.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course, Long> { //
-}
