076790fd8ab6ccf64556e6126081912645addd42
seungit <minsg3669@gmail.com>
2022-12-05 00:59:07 +0900

    Spring study - MySelectShop - AllInOne

--- myselectshopbeta/src/main/java/com/sparta/myselectshopbeta/naver/dto/ItemDto.java
+++ myselectshopbeta/src/main/java/com/sparta/myselectshopbeta/naver/dto/ItemDto.java
@@ -10,10 +10,10 @@ public class ItemDto {
     private String title;
     private String link;
     private String image;
-    private int lprice;
+    private int lprice;                                     // Dto변수에 아래 설정한 키값을 넣어준다.
 
-    public ItemDto(JSONObject itemJson) {
-        this.title = itemJson.getString("title");
+    public ItemDto(JSONObject itemJson) {                   // JSONObject를 파라미터값으로 받은 다음
+        this.title = itemJson.getString("title");       // 설정한 키값을 가져온다. (Json 형식의 items안에 있는 키값)
         this.link = itemJson.getString("link");
         this.image = itemJson.getString("image");
         this.lprice = itemJson.getInt("lprice");
--- myselectshopbeta/src/main/java/com/sparta/myselectshopbeta/naver/service/NaverApiService.java
+++ myselectshopbeta/src/main/java/com/sparta/myselectshopbeta/naver/service/NaverApiService.java
@@ -18,7 +18,7 @@ public class NaverApiService {
     public List<ItemDto> searchItems(String query) {
         RestTemplate rest = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
-        headers.add("X-Naver-Client-Id", "uIZSY5wGHzoKCjo6XKQwH");
+        headers.add("X-Naver-Client-Id", "IZSY5wGHzoKCjo6XKQwH");
         headers.add("X-Naver-Client-Secret", "2kmD6dH8_B");
         String body = "";
 
@@ -36,14 +36,14 @@ public class NaverApiService {
 
     public List<ItemDto> fromJSONtoItems(String response) { // fromJSONtoItems 메서드를 만들어서 dto로 변환.(JSONObject와 JSONArray를 사용하여 반환.)
 
-        JSONObject rjson = new JSONObject(response);
-        JSONArray items  = rjson.getJSONArray("items");
-        List<ItemDto> itemDtoList = new ArrayList<>();
+        JSONObject rjson = new JSONObject(response);        // 위의 String값을 JSONObject에 넣어줘서 JSONObject rjson 변수에 넣어준다.
+        JSONArray items  = rjson.getJSONArray("items"); // getJSONArray 메서드를 사용하여 key : items 부분을 꺼내서 JSONArray items에 넣어준다.
+        List<ItemDto> itemDtoList = new ArrayList<>();        // ItemDto 리스트에 넣어준 다음
 
-        for (int i=0; i<items.length(); i++) {
-            JSONObject itemJson = items.getJSONObject(i);
-            ItemDto itemDto = new ItemDto(itemJson);
-            itemDtoList.add(itemDto);
+        for (int i=0; i<items.length(); i++) {              // 위의 openapi에 display=15 즉 15개를 설정했기 때문에 15개의 형식으로 들어온다. items의 길이만큼 for문을 돌면서
+            JSONObject itemJson = items.getJSONObject(i);   //
+            ItemDto itemDto = new ItemDto(itemJson);        // ItemDto로 변환
+            itemDtoList.add(itemDto);                       // 리스트 형식으로 반환.
         }
 
         return itemDtoList;
